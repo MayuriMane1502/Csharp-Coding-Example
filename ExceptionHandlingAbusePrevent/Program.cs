a9f10878c3670fc534a811c27c9b7be9bd9a3e1c
MayuriMane1502 <67822253+MayuriMane1502@users.noreply.github.com>
2023-12-09 13:57:44 +0530

    Add files via upload

---
+++ ExceptionHandlingAbusePrevent/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Please enter Numerator");
+            int Numerator;
+            bool IsNumeratorConversionSuccessful =Int32.TryParse(Console.ReadLine(), out Numerator);
+            if(IsNumeratorConversionSuccessful)
+            {
+                Console.WriteLine("Please enter Denominator");
+                int Denominator;
+                bool IsDenominatorConversionSuccessful = Int32.TryParse(Console.ReadLine(),out Denominator);
+
+                if(IsDenominatorConversionSuccessful && Denominator != 0) 
+                {
+                    int Result = Numerator / Denominator;
+                    Console.WriteLine("Result - {0}", Result);
+                }
+                else
+                {
+                    if(Denominator == 0)
+                    {
+                        Console.WriteLine("Denominator zero is not possible");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Denominator should be the valid number between range {0} & {1}", Int32.MinValue, Int32.MaxValue);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Numerator should be the valid number between range {0} & {1}", Int32.MinValue, Int32.MaxValue);
+            }
+        }
+        
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ WhyEnumExampleUsingEnum/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        //create customer array
+        Customer[] customers = new Customer[3];
+        customers[0] = new Customer 
+        { 
+            Name = "Mark", 
+            Gender = Gender.Male //chnage -4 ->chng the numeric values to Gender = 1 to Gender= Gender.Male
+        };
+        customers[1] = new Customer 
+        { 
+            Name = "Jenny", 
+            Gender = Gender.Female 
+        };
+        customers[2] = new Customer 
+        { 
+            Name = "Sam", 
+            Gender = Gender.Unknown 
+        };
+
+        foreach (Customer customer in customers)
+        {
+            Console.WriteLine("Customer Name = {0} && gender = {1}", customer.Name, GetGender(customer.Gender));
+        }
+    }
+    public static string GetGender(Gender gender) //instead of passing int gender chng it to Gender gender
+        //change -3
+    {
+        switch (gender)
+        {
+            case Gender.Unknown:
+                return "Unknown";
+            case Gender.Male: 
+                return "Male";
+            case Gender.Female: 
+                return "Female";
+            default: 
+                return "Invalid data detected";
+        }
+    }
+}
+
+public enum Gender // chnage 1
+{
+    Unknown,
+    Male,
+    Female
+}
+public class Customer
+{
+    public string Name { get; set; }
+    //chnage int data type of gender to Gender // change 2
+    public Gender Gender { get; set; } // 0-unknown 1-Male 2- female
+
+}
