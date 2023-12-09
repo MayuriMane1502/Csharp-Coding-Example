4b7295ac76beb31e2992992b1b72652697089837
MayuriMane1502 <67822253+MayuriMane1502@users.noreply.github.com>
2023-12-09 13:48:52 +0530

    Add files via upload

---
+++ ExplicitInterfaceExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+interface I1
+{
+    void IterfaceMethod();
+}
+
+interface I2
+{
+    void IterfaceMethod();
+}
+
+public class Program : I1,I2
+{
+    void I1.IterfaceMethod() // exlicit method implementation
+    {
+        Console.WriteLine("Interface Method - 1 printed");
+    }
+
+    void I2.IterfaceMethod() // exlicit method implementation
+    {
+        Console.WriteLine("Interface Method - 2 printed");
+    }
+    public static void Main()
+    {
+        Program P = new Program();
+        ((I1)P).IterfaceMethod(); //casting interface explicitly to call the same method
+        ((I2)P).IterfaceMethod();
+
+        // if you don't want to use casting method then use 
+
+        I1 i1 = new Program();
+        I2 i2 = new Program();
+
+        i1.IterfaceMethod();
+        i2.IterfaceMethod();
+    }
+}
---
+++ InterfaceIntro2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+interface ICustomer1
+{
+    void Print1(); // interface members are public by default
+}
+
+interface ICustomer2 : ICustomer1
+{
+    void Print2();
+}
+
+public class Customer : ICustomer2
+{
+    public void Print2() //implementation of Interface Print method
+    {
+        Console.WriteLine("Interface Print- 2 Method");
+    }
+
+    public void Print1() 
+    {
+        Console.WriteLine("Interface Print- 1 Method");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Customer C1 = new Customer(); // create a object
+        C1.Print1();
+        C1.Print2();
+
+        ICustomer1 Cust = new Customer(); // refernce variable Icustomer1 is parent of Customer class
+        Cust.Print1();
+    }
+}
