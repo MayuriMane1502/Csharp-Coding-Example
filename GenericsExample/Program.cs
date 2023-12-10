f76ca0da41c25b4336eb1ebfabfa7d9c8eab6679
MayuriMane1502 <67822253+MayuriMane1502@users.noreply.github.com>
2023-12-10 12:26:26 +0530

    Add files via upload

---
+++ GenericsExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Mayuri
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            bool Equal = Calculator<string>.AreEqual("AB","AB"); //specify the datatype after class
+            //bool Equal = Calculator.AreEqual<int>(23, 23);
+            if(Equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
+        }
+    }
+
+    public class Calculator<T> // make a class generics 
+    {
+        //public static bool AreEqual(object Value1, object Value2) 
+        public static bool AreEqual<T>(T Value1, T Value2) //generics
+        {
+            return Value1.Equals(Value2);
+        }
+    }
+}
