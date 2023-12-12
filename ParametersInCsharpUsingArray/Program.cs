b081bc75e701c53255fa73567b29f703b4ced3ce
MayuriMane1502 <67822253+MayuriMane1502@users.noreply.github.com>
2023-12-12 15:36:10 +0530

    Add files via upload

---
+++ ParametersInCsharpUsingArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Mayuri
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //AddNumbers(10, 20); //sum = 30
+            AddNumbers(10, 20, 30, 40, 50); //sum= 150 (30,40,50 pass as object to parameter)
+            AddNumbers(10, 20, new object[] { 30, 40, 50 }); //alternate method same o/p
+        }
+
+        public static void AddNumbers(int FirstNumber, int SecondNumber, params object[] restOfNumbers)
+        {
+            int result = FirstNumber + SecondNumber;
+            if (restOfNumbers != null)
+            {
+                foreach (int i in restOfNumbers)
+                {
+                    result += i;
+                }
+                Console.WriteLine("Sum= {0}", result);
+            }
+        }
+    }
+}
